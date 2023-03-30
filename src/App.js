dcbd192cc63c692dc86faad30fd5e19d795b07b4
FinntasticSmith <114696609+FinntasticSmith@users.noreply.github.com>
2023-03-30 07:43:10 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/header"
+import Meme from "./components/Meme"
+import style from "./style.css"
+
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+
+
+export default function Header(){
+    return(
+        <header className="header">
+            <img src="https://www.pngall.com/wp-content/uploads/2016/05/Trollface-PNG.png" alt="trollface" className="header--image"></img>
+            <h2 className = "header--title">Meme Generator</h2>
+           
+        </header>
+    )
+}
